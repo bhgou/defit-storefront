27217d25ad42b353922e551d57ec22fc2f7be253
bhgou <bratihkintime@gmail.com>
2026-09-09 19:01:13 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ const STORE = 'defit.'
 const money = new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB', maximumFractionDigits: 0 })
 const emptyCart: Cart = { items: [], count: 0, subtotal: 0 }
 
-function Test(){
-  
+function Test2(){
+
 }
 
 
